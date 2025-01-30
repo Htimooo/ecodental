264d37223df9710c13fdf5b9c9508a3d8562695f
Hugo Andrés Timote <76624227+Htimooo@users.noreply.github.com>
2025-01-30 02:15:09 -0300

    cambio de logo y actualizacion de etiqueta de google

--- src/components/Tratamientos.jsx
+++ src/components/Tratamientos.jsx
@@ -9,7 +9,7 @@ const  Tratamientos =(props) => {
  
 
   return (
-    <Stack id='tratamientos' sx={{padding:'.3rem', width:'(calc 100% - .8rem)',  maxWidth: '50rem', margin:'0 auto',position: 'relative',zIndex:100 }} >
+    <Stack id='tratamientos' sx={{padding:'.3rem', width:'(calc 100% - .8rem)', height:'100% ', maxWidth: '50rem', margin:'0 auto',position: 'relative',zIndex:100 }} >
         <Typography variant='h2'sx={{fontSize:'2rem', margin:'1rem 0'}}>Nuestros Tratamientos</Typography>
         <div id='grilla'>
     {tratamientosList.map((tratamiento)=>{
